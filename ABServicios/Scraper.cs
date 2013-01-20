4db3f6a4f6b67cc53f70329d5151eee57ebe5a64
abanzas <abanzas@localhost>
2013-01-20 11:07:46 -0300

    divisas, dolar y blue desde ambito financiero

---
+++ ABServicios/Scraper.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Net;
+using System.IO;
+using HtmlAgilityPack;
+
+namespace ABServicios
+{
+	/// <summary>
+	/// This class opens a website and scraps the contents
+	/// </summary>
+	public class Scraper
+	{
+		public HtmlNode GetNodes(Uri url)
+        {
+            // Create the WebRequest for the URL we are using
+            var req = WebRequest.Create(url);
+				
+			// Get the stream from the returned web response
+			var stream = new StreamReader(req.GetResponse().GetResponseStream());
+            
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.Load(stream);
+            return htmlDocument.DocumentNode;
+        }
+	}
+}
