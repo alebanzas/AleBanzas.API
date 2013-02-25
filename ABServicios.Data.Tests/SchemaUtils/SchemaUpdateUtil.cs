8f3da5f86e18f71e5db9ddf7dfc37326c4e82b24
abanzas <abanzas@localhost>
2013-02-25 00:37:54 -0300

    empiezo servicio de SUBE

---
+++ ABServicios.Data.Tests/SchemaUtils/SchemaUpdateUtil.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
+using NUnit.Framework;
+
+namespace ABServicios.Data.Tests.SchemaUtils
+{
+	public class SchemaUpdateUtil
+	{
+		[Test]
+ 		public void SchemaUpdate()
+		{
+			var cfg = new Configuration().Configure();
+			//cfg.CreateIndexesForForeignKeys();
+
+			var sb = new StringBuilder();
+			new SchemaUpdate(cfg).Execute(s=> sb.Append(s), false);
+			Console.WriteLine(sb.ToString());
+		}
+	}
+}
---
+++ ABServicios/Controllers/SUBEController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ABServicios.BLL.DataInterfaces;
+using ABServicios.BLL.Entities;
+using Microsoft.Practices.ServiceLocation;
+
+namespace ABServicios.Controllers
+{
+    public class SUBEController : Controller
+    {
+        private readonly IRepository<RecargaSUBE> _recargaSUBERepo;
+        private readonly IRepository<VentaSUBE> _ventaSUBERepo;
+
+        public SUBEController()
+        {
+            _recargaSUBERepo = ServiceLocator.Current.GetInstance<IRepository<RecargaSUBE>>();
+            _ventaSUBERepo = ServiceLocator.Current.GetInstance<IRepository<VentaSUBE>>();
+		}
+
+        //
+        // GET: /SUBE/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
