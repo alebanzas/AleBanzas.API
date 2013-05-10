14a580a6e50f0a9a9cb5d551ac84546c59c73b58
Alejandro Banzas <alebanzas@outlook.com>
2013-05-09 22:58:36 -0300

    boca service

---
+++ ABServicios/Controllers/BocaController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using ABServicios.Attributes;
+
+namespace ABServicios.Controllers
+{
+    [NeedRelationalPersistence]
+    public class BocaController : Controller
+    {
+        //
+        // GET: /Boca/
+
+        public ActionResult Index()
+        {
+            return new HttpStatusCodeResult(200);
+            //return new HttpStatusCodeResult(404);
+        }
+    }
+}
