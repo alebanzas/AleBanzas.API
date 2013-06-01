cc19d265ddd1f9a23d43061b7d8da763c471a44b
Alejandro Banzas <alebanzas@outlook.com>
2013-06-01 13:38:35 -0300

    mejoras en API transporte

--- ABServicios/Controllers/TransporteController.cs
+++ ABServicios/Controllers/TransporteController.cs
@@ -26,84 +26,81 @@ namespace ABServicios.Controllers
         // GET: /Transporte/
 
         [NeedRelationalPersistence]
-        public ActionResult All(int cant = 100)
+        public ActionResult All(int cant = 100, bool puntos = false)
         {
-            return Json(_transportesRepo.Take(cant).ToList().Select(ConvertTo), JsonRequestBehavior.AllowGet);
+            return Json(_transportesRepo.Take(cant).ToList().Select(x => x.ToTransporteViewModel(puntos)), JsonRequestBehavior.AllowGet);
         }
 
         [NeedRelationalPersistence]
-        public ActionResult Subte()
+        public ActionResult Subte(bool puntos = false)
         {
-            return Json(_transportesRepo.Where(x => x.Tipo == TiposTransporte.Subte).ToList().Select(ConvertTo), JsonRequestBehavior.AllowGet);
+            return Json(_transportesRepo.Where(x => x.Tipo == TiposTransporte.Subte).ToList().Select(x => x.ToTransporteViewModel(puntos)), JsonRequestBehavior.AllowGet);
         }
 
         [NeedRelationalPersistence]
-        public ActionResult Tren()
+        public ActionResult Tren(bool puntos = false)
         {
-            return Json(_transportesRepo.Where(x => x.Tipo == TiposTransporte.Tren).ToList().Select(ConvertTo), JsonRequestBehavior.AllowGet);
+            return Json(_transportesRepo.Where(x => x.Tipo == TiposTransporte.Tren).ToList().Select(x => x.ToTransporteViewModel(puntos)), JsonRequestBehavior.AllowGet);
         }
 
         [NeedRelationalPersistence]
-        public ActionResult TrenPorLinea(string linea)
+        public ActionResult TrenPorLinea(string linea, bool puntos = false)
         {
-            return Json(_transportesRepo.Where(x => x.Tipo == TiposTransporte.Tren && x.Linea == linea.ToUrl()).ToList().Select(ConvertTo), JsonRequestBehavior.AllowGet);
+            return Json(_transportesRepo.Where(x => x.Tipo == TiposTransporte.Tren && x.Linea == linea.ToUrl()).ToList().Select(x => x.ToTransporteViewModel(puntos)), JsonRequestBehavior.AllowGet);
         }
 
         [NeedRelationalPersistence]
-        public ActionResult PorId(Guid id)
+        public ActionResult PorId(Guid id, bool puntos = false)
         {
-            return Json(ConvertTo(_transportesRepo.FirstOrDefault(x => x.ID == id)), JsonRequestBehavior.AllowGet);
+            return Json(_transportesRepo.FirstOrDefault(x => x.ID == id).ToTransporteViewModel(puntos), JsonRequestBehavior.AllowGet);
         }
 
         [NeedRelationalPersistence]
-        public ActionResult ColectivoPorLinea(string linea)
+        public ActionResult PorLinea(string linea, bool puntos = false)
         {
-            return Json(_transportesRepo.Where(x => x.Tipo == TiposTransporte.Colectivo && x.Linea == linea.ToUrl()).ToList().Select(ConvertTo), JsonRequestBehavior.AllowGet);
+            return Json(_transportesRepo.Where(x => x.Linea == linea.ToUrl()).ToList().Select(x => x.ToTransporteViewModel(puntos)), JsonRequestBehavior.AllowGet);
         }
 
         [NeedRelationalPersistence]
-        public ActionResult Cercano(double lat, double lon, string tipo, int cant = int.MaxValue, int caminar = 800)
+        public ActionResult ColectivoPorLinea(string linea, bool puntos = false)
         {
-            var source = new Point(lat, lon);
+            return Json(_transportesRepo.Where(x => x.Tipo == TiposTransporte.Colectivo && x.Linea == linea.ToUrl()).ToList().Select(x => x.ToTransporteViewModel(puntos)), JsonRequestBehavior.AllowGet);
+        }
 
+        [NeedRelationalPersistence]
+        public ActionResult Cercano(double lat, double lon, string tipo, int cant = int.MaxValue, int caminar = 800, bool puntos = false)
+        {
             var query = ServiceLocator.Current.GetInstance<IGetTransporteCercanoQuery>();
 
             var list = query.GetMasCercanos(new Point(lat, lon), caminar);
 
-            /*
-            IEnumerable<Transporte> transportes = _transportesRepo;
-
-            IEnumerable<Transporte> query = transportes.Where(point =>
-                                    source.Distance(point.Ubicacion) < minCaminar);
-
-            if (!string.IsNullOrWhiteSpace(tipo))
-            {
-                query = query.Where(x => x.Tipo == TiposTransporte.ByNickName(tipo));
-            }
-            var result = query.Take(cant).ToList().Select(ConvertTo);
-            */
-            var result = list.Select(ConvertTo);
+            var result = list.Select(x => x.ToTransporteViewModel(puntos));
 
             return Json(result, JsonRequestBehavior.AllowGet);
         }
+    }
 
-        private TransporteViewModel ConvertTo(Transporte transporte)
+
+    public static class TransporteExtensions
+    {
+        public static TransporteViewModel ToTransporteViewModel(this Transporte transporte, bool incluyePuntos = false)
         {
             return new TransporteViewModel
-            {
-                TipoNickName = transporte.Tipo.Nombre.ToUrl(),
-                Nombre = transporte.Nombre,
-                Linea = transporte.Linea,
-                Ramal = transporte.Ramal,
-                Puntos = transporte.Ubicacion.Coordinates.ToPuntoViewModel(),
-                RecorridoText = transporte.RecorridoText,
-            };
+                    {
+                        ID = transporte.ID,
+                        TipoNickName = transporte.Tipo.Nombre.ToUrl(),
+                        Nombre = transporte.Nombre,
+                        Linea = transporte.Linea,
+                        Ramal = transporte.Ramal,
+                        Puntos = incluyePuntos ? transporte.Ubicacion.Coordinates.ToPuntoViewModel() : new List<PuntoViewModel>(),
+                        RecorridoText = transporte.RecorridoText,
+                    };
         }
     }
 
     public static class CoordinatesExtensions
     {
-        public static List<PuntoViewModel> ToPuntoViewModel(this ICoordinate[] source)
+        public static List<PuntoViewModel> ToPuntoViewModel(this Coordinate[] source)
         {
             return source.Select(x => new PuntoViewModel
                 {
@@ -122,6 +119,8 @@ namespace ABServicios.Controllers
 
     public class TransporteViewModel
     {
+        public Guid ID { get; set; }
+
         public string TipoNickName { get; set; }
 
         public string Nombre { get; set; }
