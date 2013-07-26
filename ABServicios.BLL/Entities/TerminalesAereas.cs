07f415e4ecb3f84c265918265b3e575fbb16441b
Alejandro Banzas <alebanzas@outlook.com>
2013-07-26 01:51:21 -0300

    servicio de arribos y partidas de aeropuertos

---
+++ ABServicios.BLL/Entities/TerminalesAereas.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using AB.Common.Helpers;
+using NetTopologySuite.Geometries;
+
+namespace ABServicios.BLL.Entities
+{
+    [Serializable]
+    public class TerminalAerea
+    {
+        public string Nombre { get; set; }
+
+        public string NickName { get; set; }
+
+        public virtual Point Ubicacion { get; set; }
+
+        public string Telefono { get; set; }
+
+        public string Direccion { get; set; }
+    }
+
+    public class TerminalesAereas : ReadOnlyCollection<TerminalAerea>
+    {
+        private static readonly HashSet<TerminalAerea> _repository;
+
+        public static TerminalAerea Ezeiza = new TerminalAerea
+        {
+            Nombre = "Aeropuerto Internacional de Ezeiza \"Ministro Pistarini\"",
+            NickName = "EZE",
+            Ubicacion = new Point(-34.812393945083, -58.5363578796387),
+            Direccion = "Autopista Tte. Gral. Ricchieri Km 33,5",
+            Telefono = "(54 11) 5480 2500",
+        };
+
+        public static TerminalAerea Aeroparque = new TerminalAerea
+        {
+            Nombre = "Aeroparque Internacional \"Jorge Newbery\"",
+            NickName = "AEP",
+            Ubicacion = new Point(-34.5584560886206, 58.4167098999023),
+            Direccion = "Av. Rafael Obligado s/n°",
+            Telefono = "(54 11) 5480 6111",
+        };
+        
+        static TerminalesAereas()
+        {
+            _repository = new HashSet<TerminalAerea> { Ezeiza, Aeroparque };
+        }
+
+        public TerminalesAereas(IList<TerminalAerea> list)
+            : base(list)
+        {
+        }
+
+        public static IEnumerable<TerminalAerea> Repository
+        {
+            get { return _repository; }
+        }
+
+        public static TerminalAerea ByNickName(string nombre)
+        {
+            return _repository.FirstOrDefault(x => x.NickName.ToUrl() == nombre.ToUrl());
+        }
+    }
+}
---
+++ ABServicios/Models/AvionesStatusModel.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ABServicios.Models
+{
+    public class AvionesStatusModel
+    {
+        public AvionesStatusModel()
+        {
+            Terminales = new List<AvionesTerminalStatusModel>();
+        }
+
+        public IList<AvionesTerminalStatusModel> Terminales { get; set; }
+
+        public DateTime Actualizacion { get; set; }
+
+        public string ActualizacionStr
+        {
+            get { return string.Format("{0} {1}", Actualizacion.ToLongDateString(), Actualizacion.ToLongTimeString()); }
+        }
+    }
+
+    public class AvionesTerminalStatusModel
+    {
+        public AvionesTerminalStatusModel()
+        {
+            Partidas = new List<VueloPartidaModel>();
+            Arribos = new List<VueloArriboModel>();
+        }
+
+        public string Nombre { get; set; }
+
+        public string NickName { get; set; }
+
+        public IList<VueloPartidaModel> Partidas { get; set; }
+
+        public IList<VueloArriboModel> Arribos { get; set; }
+    }
+
+    public class VueloPartidaModel
+    {
+        public string Nombre { get; set; }
+
+        public string Linea { get; set; }
+
+        public string Destino { get; set; }
+
+        public DateTime Hora { get; set; }
+
+        public DateTime? Estima { get; set; }
+
+        public DateTime? Partida { get; set; }
+
+        public string Estado { get; set; }
+
+        public string Terminal { get; set; }
+    }
+
+    public class VueloArriboModel
+    {
+        public string Nombre { get; set; }
+
+        public string Linea { get; set; }
+
+        public string Origen { get; set; }
+
+        public DateTime Hora { get; set; }
+
+        public DateTime? Estima { get; set; }
+
+        public DateTime? Arribo { get; set; }
+
+        public string Estado { get; set; }
+
+        public string Terminal { get; set; }
+    }
+}
