98d82cab24fadf24c7fcdfd900646a3bc635f2ef
abanzas <abanzas@localhost>
2013-07-30 23:24:53 -0300

    reporting, cambio pagina de test a otra url

--- ABServicios/Controllers/HomeController.cs
+++ ABServicios/Controllers/HomeController.cs
@@ -8,25 +8,7 @@ namespace ABServicios.Controllers
     {
         public ActionResult Index()
         {
-            //var hotel = new Hotel
-            //                  {
-            //                      Nombre = "PRUEBA",
-            //                      Descripcion = DateTime.Now.ToString(CultureInfo.InvariantCulture),
-            //                      Ubicacion = new Point(1, 2),
-            //                  };
-
-            //var cfg = new Configuration().Configure();
-            //var sessionFactory = cfg.BuildSessionFactory();
-            //using (var session = sessionFactory.OpenSession())
-            //{
-            //    using (var tx = session.BeginTransaction())
-            //    {
-            //        session.SaveOrUpdate(hotel);
-
-            //        tx.Commit();
-            //    }
-            //}
-            return View();
+            return HttpNotFound();
         }
 
         public ActionResult RemoveAll()
