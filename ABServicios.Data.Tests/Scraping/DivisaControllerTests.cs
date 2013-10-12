8c79e2b374fd8eccdea2a2a547ceb68676cdf4a1
abanzas <abanzas@localhost>
2013-10-12 13:07:55 -0300

    agrego rofex como cotizacion para divisas

---
+++ ABServicios.Data.Tests/Scraping/DivisaControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ABServicios.Controllers;
+using NUnit.Framework;
+
+namespace ABServicios.Data.Tests.Scraping
+{
+    public class DivisaControllerTests
+    {
+        [Test]
+        public void WhenGetRofexModel()
+        {
+            var model = DivisaController.GetRofexModel();
+
+        }
+    }
+
+}
