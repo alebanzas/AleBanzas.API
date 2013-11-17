a93db9ae9ee818459092aca38194827e7f51124e
Alejandro Banzas <alebanzas@outlook.com>
2013-11-17 13:00:31 -0300

    data config

---
+++ ABServicios/App_Start/DataConfig.cs
@@ -0,0 +1,18 @@
+﻿using ABServicios.Controllers;
+using System.Web;
+using System.Web.Optimization;
+
+namespace ABServicios
+{
+    public class DataConfig
+    {
+        public static void StartInitial()
+        {
+            new SubteController().Start();
+            new TrenesController().Start();
+            new BicicletasController().FirstStart();
+            new AvionesController().Start();
+            new DivisaController().FirstStart();
+        }
+    }
+}
