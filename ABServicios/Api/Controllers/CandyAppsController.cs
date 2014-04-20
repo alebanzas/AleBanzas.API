c706be0cea47a983d0569740a48511e971d3dac0
Alejandro Banzas <alebanzas@outlook.com>
2014-04-20 12:13:19 -0300

    candyapps desde db

--- ABServicios/Api/Controllers/CandyAppsController.cs
+++ ABServicios/Api/Controllers/CandyAppsController.cs
@@ -1,29 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
+using ABServicios.Api.Binders;
+using ABServicios.BLL.DataInterfaces;
+using ABServicios.BLL.Entities;
+using Microsoft.Practices.ServiceLocation;
 
 namespace ABServicios.Api.Controllers
 {
     public class CandyAppsController : ApiController
     {
+        private readonly IRepository<CandyApp> _candyAppRepo;
+
+        public CandyAppsController()
+        {
+            _candyAppRepo = ServiceLocator.Current.GetInstance<IRepository<CandyApp>>();
+        }
+
+        [NeedDataBaseContext]
         public List<AppItem> Get()
         {
-            return new List<AppItem> 
+            return _candyAppRepo.Select(x => new AppItem
             {
-                new AppItem { Nombre = "App prueba 1", Imagen = "http://urldelaimagen.com/1.jpg", Url = "http://urldelaap.com/app1" },
-                new AppItem { Nombre = "App prueba 2", Imagen = "http://urldelaimagen.com/2.jpg", Url = "http://urldelaap.com/app2" },
-                new AppItem { Nombre = "App prueba 3", Imagen = "http://urldelaimagen.com/3.jpg", Url = "http://urldelaap.com/app3" },
-                new AppItem { Nombre = "App prueba 4", Imagen = "http://urldelaimagen.com/4.jpg", Url = "http://urldelaap.com/app4" },
-                new AppItem { Nombre = "App prueba 5", Imagen = "http://urldelaimagen.com/5.jpg", Url = "http://urldelaap.com/app5" },
-                new AppItem { Nombre = "App prueba 6", Imagen = "http://urldelaimagen.com/6.jpg", Url = "http://urldelaap.com/app6" },
-                new AppItem { Nombre = "App prueba 7", Imagen = "http://urldelaimagen.com/7.jpg", Url = "http://urldelaap.com/app7" },
-                new AppItem { Nombre = "App prueba 8", Imagen = "http://urldelaimagen.com/8.jpg", Url = "http://urldelaap.com/app8" },
-                new AppItem { Nombre = "App prueba 9", Imagen = "http://urldelaimagen.com/9.jpg", Url = "http://urldelaap.com/app9" },
-                new AppItem { Nombre = "App prueba 10", Imagen = "http://urldelaimagen.com/10.jpg", Url = "http://urldelaap.com/app10" },
-            };
+                Imagen = x.Imagen,
+                Nombre = x.Nombre,
+                Url = x.Url,
+            }).ToList();
         }
     }
 
