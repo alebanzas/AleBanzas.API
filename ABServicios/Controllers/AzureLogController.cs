74227728ec18fbe6a6578a385962cafd3ab34af3
ale-surface <ale-surface@localhost>
2014-04-29 22:24:55 -0300

    azure log image.

---
+++ ABServicios/Controllers/AzureLogController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Web.Mvc;
+using ABServicios.Azure.Storage.DataAccess.QueueStorage;
+using ABServicios.Azure.Storage.DataAccess.QueueStorage.Messages;
+
+namespace ABServicios.Controllers
+{
+    public class AzureLogController : Controller
+    {
+        // GET: AzureLog
+        public FilePathResult Index()
+        {
+            var httpRequest = HttpContext.Request;
+            if (httpRequest.UrlReferrer != null)
+                AzureQueue.Enqueue(new AzureChristmasVoteLog
+                {
+                    Date = DateTime.UtcNow,
+                    Referer = httpRequest.UrlReferrer.AbsoluteUri,
+                    UserId = httpRequest.UrlReferrer.DnsSafeHost,
+                    Ip = httpRequest.UserHostAddress,
+                });
+            var dir = Server.MapPath("/Content");
+            var path = Path.Combine(dir, "MS-Azure_rgb_Blk.png");
+            return base.File(path, "image/png");
+        }
+    }
+}
