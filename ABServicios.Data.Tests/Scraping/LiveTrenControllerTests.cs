e3171081323677e92cf243c686f072eecba29876
Alejandro Banzas <alebanzas@outlook.com>
2014-07-08 23:48:25 -0300

    arreglo test

--- ABServicios.Data.Tests/Scraping/LiveTrenControllerTests.cs
+++ ABServicios.Data.Tests/Scraping/LiveTrenControllerTests.cs
@@ -9,7 +9,7 @@ namespace ABServicios.Data.Tests.Scraping
         [Test]
         public void WhenGetModel()
         {
-            var model = LiveTrenController.GetModelFromMinInterior(new Uri("http://trenes.mininterior.gov.ar/v2_pg/arribos/ajax_arribos.php?ramal=1&rnd=5E5HvXkCkDz2JW0H&key=v%23v%23QTUtWp%23MpWRy80Q0knTE10I30kj%23FNyZ"), new Uri("http://trenes.mininterior.gov.ar/v2_pg/arribos/index.php?ramal=1"));
+            var model = LiveTrenController.GetModelFromMinInterior("test", new Uri("http://trenes.mininterior.gov.ar/v2_pg/arribos/ajax_arribos.php?ramal=1&rnd=5E5HvXkCkDz2JW0H&key=v%23v%23QTUtWp%23MpWRy80Q0knTE10I30kj%23FNyZ"), new Uri("http://trenes.mininterior.gov.ar/v2_pg/arribos/index.php?ramal=1"));
 
         }
 
