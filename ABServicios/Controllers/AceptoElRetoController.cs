05d3c74647346b8b3d3895fd0bc74e1e68171da2
ale-carbon <ale-carbon@localhost>
2014-07-17 18:12:53 -0300

    arranco lo de INET

---
+++ ABServicios/Controllers/AceptoElRetoController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ABServicios.Attributes;
+using ABServicios.Azure.Storage;
+using ABServicios.BLL.DataInterfaces;
+using ABServicios.BLL.Entities;
+using Microsoft.Practices.ServiceLocation;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace ABServicios.Controllers
+{
+    [NeedRelationalPersistence]
+    public class AceptoElRetoController : Controller
+    {
+        private readonly IRepository<InetForm> _inetFormRepo;
+
+        public AceptoElRetoController()
+		{
+            _inetFormRepo = ServiceLocator.Current.GetInstance<IRepository<InetForm>>();
+		}
+
+        // GET: AceptoElReto
+        public ActionResult Index(InetForm form, HttpPostedFileBase file)
+        {
+            if (form == null || string.IsNullOrWhiteSpace(form.Team) || file == null)
+            {
+                return View(form);
+            }
+            
+            var id = Guid.NewGuid();
+            var blobStorageType = AzureAccount.DefaultAccount().CreateCloudBlobClient();
+            var container = blobStorageType.GetContainerReference("inet");
+            var strings = file.FileName.Split('.');
+            var blockBlob = container.GetBlockBlobReference("inet-" + id.ToString("N") + "." + strings[strings.Length - 1]);
+            blockBlob.UploadFromStream(file.InputStream);
+            
+            form.FileUrl = blockBlob.Uri;
+            form.Date = DateTime.UtcNow;
+
+            _inetFormRepo.Add(form);
+
+            return View("Thanks");
+        }
+    }
+}
