c6a88ce3c76710eca5beff83b8a63a9f88190847
ale-carbon <ale-carbon@localhost>
2014-08-12 16:04:33 -0300

    arranco dashboard

--- AzureDashboard/Controllers/HomeController.cs
+++ AzureDashboard/Controllers/HomeController.cs
@@ -1,30 +1,136 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Web.Caching;
 using System.Web.Mvc;
+using ABServicios.Azure.Storage;
+using ABServicios.Azure.Storage.DataAccess.TableStorage;
+using ABServicios.Azure.Storage.DataAccess.TableStorage.Queries;
+using AzureDashboard.Services;
 
 namespace AzureDashboard.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private readonly WebCache cache = new WebCache();
+        public static string CacheKey = "DashBoard";
+        public static string CacheControlKey = "DashBoardControl";
+
+        public VotacionModel DefaultModel = new VotacionModel();
+
+        public readonly AzureChristmasResultQuery query;
+
+        public HomeController()
         {
-            return View();
+            query = new AzureChristmasResultQuery(AzureAccount.DefaultAccount());
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
 
-            return View();
+        public ActionResult Index(string version = "1", string type = "ALL")
+        {
+            return View(cache.Get<VotacionModel>(CacheKey) ?? DefaultModel);
         }
 
-        public ActionResult Contact()
+        public EmptyResult FirstStart()
         {
-            ViewBag.Message = "Your contact page.";
+            try
+            {
+                var result = GetModel();
+                cache.Put(CacheKey, result, new TimeSpan(1, 0, 0, 0));
+            }
+            catch (Exception)
+            {
 
-            return View();
+            }
+            return Start();
         }
+
+        public EmptyResult Start()
+        {
+            cache.Put(CacheControlKey, new VotacionModel(), new TimeSpan(0, 2, 0), CacheItemPriority.NotRemovable,
+                (key, value, reason) =>
+                {
+                    try
+                    {
+                        var result = GetModel();
+                        cache.Put(CacheKey, result, new TimeSpan(1, 0, 0, 0));
+                    }
+                    catch (Exception ex)
+                    {
+                    }
+                    finally
+                    {
+                        Start();
+                    }
+                });
+            return new EmptyResult();
+        }
+
+        private VotacionModel GetModel()
+        {
+            IEnumerable<IGrouping<string, AzureChristmasVoteLogData>> result = query.GetResults();
+
+            var rr = new VotacionModel();
+
+            foreach (IGrouping<string, AzureChristmasVoteLogData> item in result.OrderByDescending(x => x.Count()))
+            {
+                if (item.Key.ToLowerInvariant().Contains("google")) continue;
+
+                int count = item.Distinct(new AzureChristmasVoteLogDataComparer()).Count();
+                
+                rr.Lista.Add(new VotacionItem
+                {
+                    Nombre = item.Key,
+                    Count = count,
+                });
+            }
+
+            rr.Lista = rr.Lista.OrderByDescending(x => x.Count).ToList();
+
+            return rr;
+        }
+
+    }
+
+    public class AzureChristmasVoteLogDataComparer : IEqualityComparer<AzureChristmasVoteLogData>
+    {
+        // Products are equal if their names and log numbers are equal. 
+        public bool Equals(AzureChristmasVoteLogData x, AzureChristmasVoteLogData y)
+        {
+
+            //Check whether the compared objects reference the same data. 
+            if (Object.ReferenceEquals(x, y)) return true;
+
+            //Check whether any of the compared objects is null. 
+            if (Object.ReferenceEquals(x, null) || Object.ReferenceEquals(y, null))
+                return false;
+
+            //Check whether the products' properties are equal. 
+            return x.Ip == y.Ip && x.Date.Date == y.Date.Date;
+        }
+
+        // If Equals() returns true for a pair of objects  
+        // then GetHashCode() must return the same value for these objects. 
+
+        public int GetHashCode(AzureChristmasVoteLogData log)
+        {
+            return log.Date.Date.GetHashCode() + log.Ip.GetHashCode();
+        }
+
+    }
+
+    public class VotacionModel
+    {
+        public VotacionModel()
+        {
+            Lista = new List<VotacionItem>();
+        }
+        public List<VotacionItem> Lista { get; set; }
+    }
+
+    public class VotacionItem
+    {
+        public string Nombre { get; set; }
+        public int Count { get; set; }
     }
 }
