a66082a10f46ddc812432d14df49566345a5754f
ale-carbon <ale-carbon@localhost>
2015-04-05 02:51:56 -0300

    arreglo queries a tablas

--- ABServicios.Azure.Storage/DataAccess/TableStorage/Queries/LocationAccessLogQuery.cs
+++ ABServicios.Azure.Storage/DataAccess/TableStorage/Queries/LocationAccessLogQuery.cs
@@ -2,29 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
 using Microsoft.WindowsAzure.Storage.Table.DataServices;
 
 namespace ABServicios.Azure.Storage.DataAccess.TableStorage.Queries
 {
 	public class LocationAccessLogQuery
 	{
-		private readonly TableServiceContext _tableContext;
-        private readonly TablePersister<ApiAccessLogData> _tablePersister;
+	    private readonly CloudTable _table;
 
-        public LocationAccessLogQuery(CloudStorageAccount account)
+	    public LocationAccessLogQuery(CloudStorageAccount account)
 		{
 		    var client = account.CreateCloudTableClient();
-			_tableContext = client.GetTableServiceContext();
-            _tablePersister = new TablePersister<ApiAccessLogData>(client);
+            _table = client.GetTableReference(typeof(ApiAccessLogData).AsTableStorageName());
 		}
         
 		public IList<ApiAccessLogData> GetResultsFromDate(DateTime date)
 		{
             var pk = date.ToString("yyyyMMdd");
 
-            var queryable = _tableContext.CreateQuery<ApiAccessLogData>(typeof(ApiAccessLogData).AsTableStorageName());
+            var queryable = _table.CreateQuery<ApiAccessLogData>();
 
-			var result = (from data in queryable where data.PartitionKey == pk select data).AsTableServiceQuery(_tableContext).Execute();
+			var result = (from data in queryable where data.PartitionKey == pk select data);
 
 		    return result.ToList();
 		}
--- ABServicios.Azure.Storage/DataAccess/TableStorage/Queries/SummaryAccessLogQuery.cs
+++ ABServicios.Azure.Storage/DataAccess/TableStorage/Queries/SummaryAccessLogQuery.cs
@@ -2,29 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
 using Microsoft.WindowsAzure.Storage.Table.DataServices;
 
 namespace ABServicios.Azure.Storage.DataAccess.TableStorage.Queries
 {
 	public class SummaryAccessLogQuery
 	{
-		private readonly TableServiceContext _tableContext;
-        private readonly TablePersister<ApiAccessLogData> _tablePersister;
+		private readonly CloudTable _table;
 
         public SummaryAccessLogQuery(CloudStorageAccount account)
 		{
 		    var client = account.CreateCloudTableClient();
-			_tableContext = client.GetTableServiceContext();
-            _tablePersister = new TablePersister<ApiAccessLogData>(client);
+            _table = client.GetTableReference(typeof (ApiAccessLogData).AsTableStorageName());
 		}
         
 		public IEnumerable<IGrouping<string, ApiAccessLogData>> GetResultsFromDate(DateTime date)
 		{
             var pk = date.ToString("yyyyMMdd");
 
-            var queryable = _tableContext.CreateQuery<ApiAccessLogData>(typeof(ApiAccessLogData).AsTableStorageName());
+            var queryable = _table.CreateQuery<ApiAccessLogData>();
 
-			var result = (from data in queryable where data.PartitionKey == pk select data).AsTableServiceQuery(_tableContext).Execute();
+			var result = (from data in queryable
+                            where data.PartitionKey == pk
+                          select data);
 
 		    var list = result.ToList();
 
