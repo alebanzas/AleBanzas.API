a47a31b44e7f5b3e9ed51c34a41ac00d7427061a
ale-carbon <ale-carbon@localhost>
2015-10-20 19:13:57 -0300

    pagina de concurso

---
+++ AzureContest.Web/Controllers/ImagenController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AzureContest.Web.Controllers
+{
+    public class ImagenController : Controller
+    {
+        // GET: Imagen
+        public ActionResult Index(string r)
+        {
+            object re = string.IsNullOrWhiteSpace(r) ? "alebanzas" : r.Replace(" ", "").Replace("=", "").Replace("&", "").Replace("?", "").Replace("\\", "").Replace("\"", "");
+
+            return View("Index", re);
+        }
+    }
+}
---
+++ AzureContest.Web/Controllers/SitioWebController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AzureContest.Web.Controllers
+{
+    public class SitioWebController : Controller
+    {
+        // GET: SitioWeb
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
