f1da0fea6a556a7c93b52ee7f3bb003f72e03479
Alejandro Banzas <alebanzas@outlook.com>
2015-10-20 19:13:57 -0300

    pagina de concurso

---
+++ AzureContest.Web/Controllers/BasesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AzureContest.Web.Controllers
+{
+    public class BasesController : Controller
+    {
+        // GET: Bases
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AzureContest.Web/Controllers/CodigoController.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Http;
+using System.Web.Mvc;
+
+namespace AzureContest.Web.Controllers
+{
+    public class CodigoController : Controller
+    {
+        // GET: Codigo
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Get(string nombre, string apellido, string email)
+        {
+            var httpClient = new HttpClient();
+
+            var request = httpClient.GetAsync($"http://api.alebanzas.com.ar/api/dreamspark?nombre={nombre}&apellido={apellido}&email={email}");
+
+            var codigo = request.Result.Content.ReadAsStringAsync().Result;
+
+            return PartialView("Get", codigo);
+        }
+
+    }
+}
