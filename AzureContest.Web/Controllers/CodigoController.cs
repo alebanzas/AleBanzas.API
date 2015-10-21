a85039e7d4fa4faee02dccb420292e79790e7ab7
Alejandro Banzas <alebanzas@outlook.com>
2015-10-21 19:24:26 -0300

    fix a calidad de sitioweb

--- AzureContest.Web/Controllers/CodigoController.cs
+++ AzureContest.Web/Controllers/CodigoController.cs
@@ -1,5 +1,7 @@
-﻿using System.Net.Http;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net.Http;
 using System.Web.Mvc;
+using reCAPTCHA.MVC;
 
 namespace AzureContest.Web.Controllers
 {
@@ -8,11 +10,22 @@ namespace AzureContest.Web.Controllers
         // GET: Codigo
         public ActionResult Index()
         {
-            return View();
+            return View("Index", new CodigoModel());
         }
-        
+
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [CaptchaValidator(ErrorMessage = "¿Sos un robot?")]
+        public ActionResult Index(CodigoModel form)
+        {
+            if (ModelState.IsValid)
+            {
+                return Get(form.Nombre, form.Apellido, form.Email);
+            }
+
+            return PartialView("Form", form);
+        }
+
         public ActionResult Get(string nombre, string apellido, string email)
         {
             var httpClient = new HttpClient();
@@ -24,5 +37,18 @@ namespace AzureContest.Web.Controllers
             return PartialView("Get", codigo);
         }
 
+    }
+
+    public class CodigoModel
+    {
+        [Required(ErrorMessage = "*")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        public string Apellido { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [EmailAddress(ErrorMessage = "Email inválido")]
+        public string Email { get; set; }
     }
 }
