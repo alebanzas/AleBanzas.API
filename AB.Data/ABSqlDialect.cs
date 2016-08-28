fdb285a5aed05494131bdc52da18e634fb4213ff
Alejandro Banzas <alebanzas@outlook.com>
2016-08-28 17:50:36 -0300

    nhibernate spatial dialect y geographytype

--- AB.Data/ABSqlDialect.cs
+++ AB.Data/ABSqlDialect.cs
@@ -1,11 +1,12 @@
 ﻿using NHibernate;
 using NHibernate.Dialect;
 using NHibernate.Dialect.Function;
+using NHibernate.Spatial.Dialect;
 
 namespace AB.Data
 {
     //NHibernate.Spatial.Dialect.MsSql2008GeometryDialect, NHibernate.Spatial.MsSql2008
-    public class ABSqlDialect : MsSqlAzure2008Dialect
+    public class ABSqlDialect : MsSql2012GeographyDialect
 	{
 		public ABSqlDialect()
 		{
